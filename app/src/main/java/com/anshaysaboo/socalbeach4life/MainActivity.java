82d2e0b1d2fb320fec78780cdb1f8483b6e0b829
Anshay Saboo <anshaysaboo1586@gmail.com>
2022-10-13 22:21:50 -0700

    Added firebase and google maps dependencies, set up MapActivity to display user location and surrounding area

--- app/src/main/java/com/anshaysaboo/socalbeach4life/MainActivity.java
+++ app/src/main/java/com/anshaysaboo/socalbeach4life/MainActivity.java
@@ -2,7 +2,10 @@ package com.anshaysaboo.socalbeach4life;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent i = new Intent(MainActivity.this, MapActivity.class);
+        MainActivity.this.startActivity(i);
     }
 }
---
+++ app/src/main/java/com/anshaysaboo/socalbeach4life/MapActivity.java
@@ -0,0 +1,86 @@
+package com.anshaysaboo.socalbeach4life;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private FusedLocationProviderClient locationProviderClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        locationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        setupPermission();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMyLocationEnabled(true);
+        // Zoom map into current location of the user
+        locationProviderClient.getLastLocation().addOnSuccessListener(this, location -> {
+            if (location != null) {
+                LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 9));
+            }
+        });
+    }
+
+    // Setup the initial map features
+    void setupMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    // Request the permissions from the user
+    void setupPermission() {
+        ActivityResultLauncher<String[]> locationPermissionRequest =
+                registerForActivityResult(new ActivityResultContracts
+                                .RequestMultiplePermissions(), result -> {
+                            Boolean fineLocationGranted = result.getOrDefault(
+                                    Manifest.permission.ACCESS_FINE_LOCATION, false);
+                            Boolean coarseLocationGranted = result.getOrDefault(
+                                    Manifest.permission.ACCESS_COARSE_LOCATION,false);
+                            if (fineLocationGranted != null && fineLocationGranted) {
+                                setupMap();
+                                Log.d("ANSHAY", "PRECISE GRANTED");
+                            } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                                Log.d("ANSHAY", "COARSE GRANTED");
+                            } else {
+                                Log.d("ANSHAY", "NOT GRANTED");
+                            }
+                        }
+                );
+        locationPermissionRequest.launch(new String[] {
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        });
+    }
+}
