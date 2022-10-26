4d833da302583a3f9ebe6709806988823bbce93b
Siddhant Bajaj <77747740+siddhant-b@users.noreply.github.com>
2022-10-26 13:47:58 -0700

    Created LoginActivity and added XML layout

---
+++ app/src/main/java/com/anshaysaboo/socalbeach4life/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.anshaysaboo.socalbeach4life;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
