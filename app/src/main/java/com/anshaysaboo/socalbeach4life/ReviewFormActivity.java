0a68849d136e410558664ee37d5e8dc7c787be85
Siddhant Bajaj <77747740+siddhant-b@users.noreply.github.com>
2022-11-03 21:44:12 -0700

    Added ReviewForm

---
+++ app/src/main/java/com/anshaysaboo/socalbeach4life/ReviewFormActivity.java
@@ -0,0 +1,13 @@
+package com.anshaysaboo.socalbeach4life;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReviewFormActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reviewform);
+    }
+}
